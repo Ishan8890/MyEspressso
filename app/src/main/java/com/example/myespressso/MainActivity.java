320bed66f02d44d5f42d5c8a16955ab55e66548f
Ishan <ishanb30@gmail.com>
2020-03-23 20:33:25 +0530

    Added comment .

--- app/src/main/java/com/example/myespressso/MainActivity.java
+++ app/src/main/java/com/example/myespressso/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Added comment to check data upload in github
     }
 }
